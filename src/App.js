8af9358cc2ec0a765e58e26835df2870df12b9e8
Naomi <151114635+naomi-ho@users.noreply.github.com>
2024-04-11 18:40:15 +1000

    Add explanatory comments to files

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
-export default function Square() {
+// this creates a component
+// export makes the Square function accessible outside of this file
+// default tells other files using the code that it's the main function in the file
+export default function Square() { 
+  // <button> is a JSX element
+  // className="square" is a button property/prop that tells CSS how to style the button
   return <button className="square">X</button>;
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles.css";
+// index.js is the bridge between the component creates in App.js and the web browser
 
-import App from "./App";
+// brings all the necessary pieces together
+import React, { StrictMode } from "react"; // react
+import { createRoot } from "react-dom/client"; // react's library to talk to web browsers (react DOM)
+import "./styles.css"; // styles for components
 
+import App from "./App"; // component created in App.js
+
+// brings all the pieces together and injects the final product into index.html in the public folder
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
