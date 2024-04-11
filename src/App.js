3baf15f06788b862e52a6188dbb39a0256752524
Naomi <151114635+naomi-ho@users.noreply.github.com>
2024-04-11 19:27:37 +1000

    Pass data through props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-// this creates a component
-// export makes the Square function accessible outside of this file
+// use props to pass the value each square should have from the parent component (Board) to its child (Square)
+function Square({ value }) {
+  // <button> is a JSX element
+  // className="square" is a button prop that tells CSS how to style the button
+  return <button className="square">{value}</button>; // escape into JS from JSX using curly braces to get an empty board and not get 'value' text
+}
+
+// export makes the Board function accessible outside of this file
 // default tells other files using the code that it's the main function in the file
-export default function Square() { 
+export default function Board() { 
   return (
     <> {/* fragments are used to return a single JSX element by wrapping multiple adjacent JSX elements */}
-      <div className="board-row"> {/* group squares into rows to make a grid structure */}
-        <button className="square">1</button> {/* <button> is a JSX element */}
-        <button className="square">2</button> {/* className="square" is a button prop that tells CSS how to style the button */}
-        <button className="square">3</button>
+      <div className="board-row"> {/* group squares into rows to make a row */}
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </>
   );
-
-  return <button className="square">X</button>;
 }
